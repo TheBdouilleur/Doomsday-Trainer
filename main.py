08e7e29ce7a83f335dd79f348564f289a97a6925
TheBdouilleur <bdouilleur@gmail.com>
2020-04-19 17:40:23 +0200

    Quick fix to re-enable background.pyw; Rename gui to main

--- main.py
+++ main.py
@@ -1,28 +1,72 @@
-import doomsday, logging, time, random
+# TODO. resultLabel bg = green when correct, else red
+# TODO. proper 'Play again' method
+
+import doomsday, logging # Common Conway date operations
+
+import kivy  # Kivy different modules
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty, StringProperty, DictProperty
+
+
+class StartWindow(Screen):
+    pass
+
+class DateWindow(Screen):
+    pass
+
+class ResultWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+
+date = "Hello"
+WIDTH = 400
+HEIGHT = 200
+
+kivy.require('1.10.1')
+Config.set('graphics', 'width', str(WIDTH))
+Config.set('graphics', 'height', str(HEIGHT))
+Config.write()
 
 logging.basicConfig(filename='average.log', level=logging.DEBUG)
 
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+class Trainer(App):
+    random_date = StringProperty("")
+    random_date = doomsday.randomDate()
 
-
-def randomDate():
-    dateList = [str(random.randint(1,28)), str(random.choice(months)), str(random.randint(1582, 2300))] # (Start of Gregorian calendar, end of useful dates)
-    date = ' '.join(dateList)
-    return date
-
-def askDate():
-    ranDate = randomDate()
-    t1 = time.perf_counter()
-    a = input('What day was the {} ?'.format(ranDate))
-    t = time.perf_counter() - t1
-    r = doomsday.dayFromDate(ranDate)
+    results = DictProperty("")
+    results = {'text': '', 'correct':None}
     
-    if int(a) == r:
-        print('Congrats! Ran in ', t)
-        logging.info('New perf: {0} answered in {1} seconds!'.format(ranDate, t))
-    else:
-        print('False: real answer was', r)
-        logging.info(' {0} False {1} in {2} seconds: real answer: {3}'.format(a, ranDate, t, r))
+    answer = StringProperty("")
+    
+
+    def build(self):
+        return Builder.load_file("trainer.kv")
+
+    def generate_results(self): # TODO reformat function more pythonically (results(self, answer))
+        day = str(doomsday.dayFromDate(self.random_date))
+    
+        if self.answer == day:
+            self.results['correct'] = True
+            self.results['text'] = 'Congrats!'
+            logging.info('New perf: {0} answered in s seconds!'.format(self.random_date))
+        
+        else:
+            self.results['correct'] = True
+            self.results['text'] = 'False: real answer was ' + day
+            logging.info(' {0} False {1} in s seconds: real answer: {2}'.format(self.random_date, self.results, day))
+
+        self.root.get_screen('third').ids.resultLabel.text = self.results['text'] #Update resultLabel's text
+        print('Label text after: ', self.root.get_screen('third').ids.resultLabel.text)
+
 
 if __name__ == "__main__":
-    askDate()
+    Trainer().run()
+
